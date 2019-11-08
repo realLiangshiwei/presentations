bcf173b687f67ef586eed4a668a24a0e6cc5c1f6
liangshiwei <deawback@hotmail.com>
2019-11-08 14:51:40 +0800

    add smaples

---
+++ 2019-11-09 China DotNetConf/samples/src/BookStore.Web/Pages/BookStorePage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using BookStore.Localization;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace BookStore.Web.Pages
+{
+    /* Inherit your UI Pages from this class. To do that, add this line to your Pages (.cshtml files under the Page folder):
+     * @inherits BookStore.Web.Pages.BookStorePage
+     */
+    public abstract class BookStorePage : AbpPage
+    {
+        [RazorInject]
+        public IHtmlLocalizer<BookStoreResource> L { get; set; }
+    }
+}
